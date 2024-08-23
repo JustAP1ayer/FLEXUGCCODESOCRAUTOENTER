e64f95a343025c85277af4453d8ab16e018a88e5
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-08-22 20:33:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 import time
-import pyperclip
-import pyautogui
-from pywinauto.keyboard import send_keys
+import os
+
+try:
+    import pyperclip
+    import pyautogui
+    from pywinauto.keyboard import send_keys
+except ImportError:
+    os.system("pip install pyautogui")
+    os.system("pip install opencv-python")
+    os.system("pip install pyperclip")
+    os.system("pip install pywinauto")
 
 def check_clipboard():
     prev_content = ""
     copied = False
+    
     while True:
         try:
-            buy = pyautogui.locateCenterOnScreen('buy.png', confidence=0.7) 
-            buy2 = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.7) 
-            redeem = pyautogui.locateCenterOnScreen('redeem.png', confidence=0.7) 
+            buy_button = pyautogui.locateCenterOnScreen('buy.png', confidence=0.7) 
+            buy_button_alt = pyautogui.locateCenterOnScreen('buy2.png', confidence=0.7) 
+            # redeem_button = pyautogui.locateCenterOnScreen('redeem.png', confidence=0.7) 
             current_content = pyperclip.paste()
-            if current_content != prev_content and (buy or buy2):
-                if buy:
-                    pyautogui.moveTo(buy)
-                else:
-                    pyautogui.moveTo(buy2)
 
+            if current_content != prev_content and (buy_button or buy_button_alt):
+                button_to_click = buy_button if buy_button else buy_button_alt
+                pyautogui.moveTo(button_to_click)
                 pyautogui.click(clicks=5)
-                if copied == False:
+
+                if not copied:
                     pyautogui.hotkey("ctrl", "v")
                     time.sleep(0.15)
                     send_keys('{ENTER}')
-
                     copied = True
-                '''if redeem:
-                    pyautogui.moveTo(redeem)
-                    pyautogui.click(clicks=1)
-                    time.sleep(0.1)
-                    pyautogui.click(clicks=1)'''
+
                 prev_content = current_content
                 copied = False
-                if buy:
-                    pyautogui.moveTo(buy)
-                else:
-                    pyautogui.moveTo(buy2)
 
+                pyautogui.moveTo(button_to_click)
                 pyautogui.click(clicks=2)
+
             time.sleep(0.1)
+
         except Exception as e:
             print(f"An error occurred: {e}")
 
